4002b7ae35ca00429c11490f20485a8db125a8dd
Michael Harris <xero5k@yahoo.com>
2021-09-09 20:50:46 -0500

    completed assignement

--- index.js
+++ index.js
@@ -21,3 +21,100 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    let employeeRecord = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord
+}//closes createEmployeeRecord
+
+function createEmployeeRecords(eRecord){
+    return eRecord.map(record => createEmployeeRecord(record))
+}//Closes createEmployeeRecords
+
+function createTimeInEvent(dateStamp){
+    let time = dateStamp.split(' ')
+    const newEvent = {
+    }
+    newEvent.type = "TimeIn"
+    newEvent.hour = parseInt(time[1])
+    newEvent.date = time[0]
+    this.timeInEvents.push(newEvent)
+
+    //console.log(newEvent)
+    return this
+
+}//closes createTimeInEvents
+
+function createTimeOutEvent(dateStamp){
+    let time = dateStamp.split(' ')
+    const newEvent = {
+    }
+    newEvent.type = "TimeOut"
+    newEvent.hour = parseInt(time[1])
+    newEvent.date = time[0]
+    this.timeOutEvents.push(newEvent)
+
+    //console.log(this)
+    return this
+    
+}//closes createTimeOutEvent
+
+function hoursWorkedOnDate(date){
+    
+    let timeIn = this.timeInEvents.find((event) => event.date === date)
+    let timeOut = this.timeOutEvents.find((event) => event.date === date)
+    let hours = (timeOut.hour - timeIn.hour)/100
+    console.log(timeIn)
+    console.log(timeOut)
+    console.log(hours)
+    return hours
+
+}//Closes hoursWorkedOnDate
+
+function wagesEarnedOnDate(date){
+
+  //  console.log(this)
+    let pay = this.payPerHour 
+    //console.log("pay: ",pay)
+    let hours = hoursWorkedOnDate.call(this, date)
+   // console.log("hours: ",hours)
+    let wage = pay * hours
+   // console.log("wage: ",wage)
+
+    return wage
+}//Closes wagesEarnedOnDate
+
+function findEmployeeByFirstName(srcArray, firstName){
+
+    for(let i in srcArray){
+        if(srcArray[i].firstName===firstName){
+            //console.log(srcArray[i])
+            return srcArray[i]
+        }
+    }
+   /*  let name = this.firstName.find((event) => event.firstName === firstName)
+    console.log("name: ",name)
+    srcArray.apply(element => {srcArray[element].firstName === firstName
+        console.log(srcArray)
+        return srcArray[element]
+    }) */ 
+}//Closes findEmployeeByFirstName
+
+function calculatePayroll(records){ 
+    return records.reduce(function(payroll, element){ 
+       return payroll + allWagesFor.call(element)
+    },0)
+    /* console.log("records: ",records)
+    let payroll = allWagesFor.apply(this, records)
+    console.log("timeIn: ",this.timeInEvents)
+    console.log(payroll)
+    return payroll */
+ 
+}//closes calculatePayroll 
+ 
